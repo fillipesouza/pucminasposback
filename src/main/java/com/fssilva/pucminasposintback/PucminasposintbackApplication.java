8f58cc91de27ff6ed1006090259a1539a5f241b0
Fillipe Silva <f.silva@f-silva08.la.corp.samsungelectronics.net>
2023-10-05 21:29:28 -0300

    First commit - setup project using java spring

---
+++ src/main/java/com/fssilva/pucminasposintback/PucminasposintbackApplication.java
@@ -0,0 +1,13 @@
+package com.fssilva.pucminasposintback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PucminasposintbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PucminasposintbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fssilva/pucminasposintback/PucminasposintbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fssilva.pucminasposintback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PucminasposintbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
