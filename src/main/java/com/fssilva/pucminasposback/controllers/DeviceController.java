175317f967452af1f2a5e3dcd6b1cb4c37d71b9d
Fillipe Silva <f.silva@f-silva08.la.corp.samsungelectronics.net>
2023-10-15 19:06:55 -0300

    Adding deviceAdmin service

--- src/main/java/com/fssilva/pucminasposback/controllers/DeviceController.java
+++ src/main/java/com/fssilva/pucminasposback/controllers/DeviceController.java
@@ -4,6 +4,7 @@ import com.fssilva.pucminasposback.models.DeviceAdmin;
 import com.fssilva.pucminasposback.models.DeviceResponseDto;
 import com.fssilva.pucminasposback.models.DeviceOrderServiceDto;
 import com.fssilva.pucminasposback.models.Devices;
+import com.fssilva.pucminasposback.services.DeviceAdminService;
 import com.fssilva.pucminasposback.services.DeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,14 +22,29 @@ public class DeviceController {
     @Autowired
     DeviceService deviceService;
 
+    @Autowired
+    DeviceAdminService deviceAdminService;
+
     @GetMapping("/{deviceId}")
     public List<DeviceResponseDto> getDeviceReadData(@PathVariable("deviceId") String deviceId) throws Exception {
-
-
         List<Devices> deviceList = deviceService.findRepliesByDeviceOnlyPostedWithinOneWeek(deviceId);
         return deviceList.stream().map(DeviceResponseDto::convertFromDevice).collect(Collectors.toList());
     }
 
+    @GetMapping("/admin/{deviceId}")
+    public DeviceAdmin getDeviceData(@PathVariable("deviceId") String deviceId) throws Exception {
+        return deviceAdminService.getDeviceAdmin(deviceId);
+    }
+
+    @PutMapping("/admin/{deviceId}")
+    public DeviceAdmin editDeviceData(@PathVariable("deviceId") String deviceId, @RequestBody DeviceAdmin deviceAdmin) throws Exception {
+        return deviceAdminService.edit(deviceId, deviceAdmin);
+    }
+
+    @PutMapping("/admin")
+    public void addDeviceData(@RequestBody DeviceAdmin deviceAdmin) throws Exception {
+         deviceAdminService.saveDeviceAdmin(deviceAdmin);
+    }
 
     @GetMapping("/summary/{customerId}")
     public List<DeviceResponseDto> getDeviceSummaryStatusByCustomer(@PathVariable("customerId") String customerId) throws Exception {
---
+++ src/main/java/com/fssilva/pucminasposback/services/DeviceAdminService.java
@@ -0,0 +1,54 @@
+package com.fssilva.pucminasposback.services;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fssilva.pucminasposback.models.DeviceAdmin;
+import com.fssilva.pucminasposback.models.DeviceOrderServiceDto;
+import com.fssilva.pucminasposback.models.Devices;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@Service
+public class DeviceAdminService {
+
+    public DeviceAdmin getDeviceAdmin(String deviceId) throws Exception {
+        AmazonDynamoDB dynamoDBClient = new AmazonDynamoDBClient();
+        DynamoDBMapper mapper = new DynamoDBMapper(dynamoDBClient);
+        return mapper.load(DeviceAdmin.class, deviceId);
+
+    }
+
+    public DeviceAdmin edit(String deviceId, DeviceAdmin deviceAdmin) throws Exception {
+        AmazonDynamoDB dynamoDBClient = new AmazonDynamoDBClient();
+        DynamoDBMapper mapper = new DynamoDBMapper(dynamoDBClient);
+        DeviceAdmin savedDeviceAdmin = mapper.load(DeviceAdmin.class, deviceId);
+        savedDeviceAdmin.setDeviceAlias(deviceAdmin.getDeviceAlias());
+        savedDeviceAdmin.setDeviceBrand(deviceAdmin.getDeviceBrand());
+        savedDeviceAdmin.setDeviceType(deviceAdmin.getDeviceType());
+        savedDeviceAdmin.setCustomerName(deviceAdmin.getCustomerName());
+        savedDeviceAdmin.setCustomerId(deviceAdmin.getCustomerId());
+        savedDeviceAdmin.setProviderBusinessNumber(deviceAdmin.getProviderBusinessNumber());
+        savedDeviceAdmin.setProviderId(deviceAdmin.getProviderId());
+        savedDeviceAdmin.setProviderName(deviceAdmin.getProviderName());
+        savedDeviceAdmin.setInstallationTime(deviceAdmin.getInstallationTime());
+        mapper.save(savedDeviceAdmin);
+        return mapper.load(DeviceAdmin.class, deviceId);
+    }
+
+    public void saveDeviceAdmin(DeviceAdmin deviceAdmin) throws Exception {
+        AmazonDynamoDB dynamoDBClient = new AmazonDynamoDBClient();
+        DynamoDBMapper mapper = new DynamoDBMapper(dynamoDBClient);
+        mapper.save(deviceAdmin);
+
+    }
+}
